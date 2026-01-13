f13e2c984f5a41815a545b410eed362d4c52e673
Yehor <gamersirius053@gmail.com>
2026-01-13 10:08:57 +0300

    solution of py-copy-file 2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     cp_lst = command.split()
-    if len(cp_lst) == 3 and cp_lst[1] != cp_lst[2] and "cp" in cp_lst:
+    if len(cp_lst) == 3 and cp_lst[1] != cp_lst[2] and "cp" in cp_lst[0]:
         try:
             with (open(cp_lst[1], "rb") as file_in,
                   open(cp_lst[2], "wb") as file_out):
