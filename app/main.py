1f4968b70c4689408a3fefc78c41e239de45bad0
Yehor <gamersirius053@gmail.com>
2026-01-13 10:20:26 +0300

    solution of py-copy-file 3

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 def copy_file(command: str) -> None:
     cp_lst = command.split()
-    if len(cp_lst) == 3 and cp_lst[1] != cp_lst[2] and "cp" in cp_lst[0]:
+    if len(cp_lst) == 3 and cp_lst[1] != cp_lst[2] and cp_lst[0] == "cp":
         try:
             with (open(cp_lst[1], "rb") as file_in,
                   open(cp_lst[2], "wb") as file_out):
                 for line in file_in:
                     file_out.write(line)
         except FileNotFoundError:
-            print("Ошибка: файл не найден.")
+            pass
